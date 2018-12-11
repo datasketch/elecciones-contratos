0ce21b28f0bfc30230e974750c23285e8a4815c3
David Daza <03dazal@gmail.com>
2018-12-11 10:50:55 -0500

    Agrega notificacion de sitio en construccion

---
+++ components/Notification.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="notification">
+    <div class="notification__inner">
+      <p>Este es un sitio en construcción. Pueden existir inconsistencias en algunos datos. Si usas esta información, asegúrese de contrastarla con las fuentes originales.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification'
+}
+</script>
+
+<style>
+.notification {
+  background: #110066;
+  color: white;
+  font-size: small;
+  text-align: center;
+}
+
+.notification__inner {
+  padding: 10px 5%;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="wrapper" class="column">
+    <Notification />
     <Nav />
     <Sidebar />
     <nuxt/>
@@ -11,9 +12,10 @@
 import Footer from '~/components/Footer'
 import Nav from '~/components/Nav'
 import Sidebar from '~/components/Sidebar'
+import Notification from '~/components/Notification'
 
 export default {
-  components: { Footer, Nav, Sidebar }
+  components: { Footer, Nav, Sidebar, Notification }
 }
 </script>
 
